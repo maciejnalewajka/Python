d7d5b85be37d10d6ed6dff29a4db6626ca84a82a
Maciej Nalewajka <nalewajkamaciek@gmail.com>
2020-02-14 12:07:56 +0100

    Add NNAH.py
    NWD
    NWW
    Anagram
    Hanoi

---
+++ NNAH.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+#NWD
+
+def gcd(a, b):
+    while b != 0:
+        b, a = a % b, b
+    return a
+
+
+# In[8]:
+
+
+# NWW
+
+def lcm(a, b):
+    return abs( a * b / gcd(a, b) )
+
+
+# In[9]:
+
+
+# Hanoi
+
+def hanoi(n, source, helper, target):
+    if n > 0:
+        hanoi(n - 1, source, target, helper)
+        if source:
+            target.append(source.pop())
+        hanoi(n - 1, helper, source, target)
+
+
+# In[10]:
+
+
+# Anagram
+
+def delSpac(s):
+    s_new = s.replace(" ","")
+    return s_new
+
+def anagram(s1,s2):
+    alist1 = list(delSpac(s1))
+    alist2 = list(delSpac(s2))
+    alist1.sort()
+    alist2.sort()
+    pos = 0
+    matches = True
+    while pos < len(delSpac(s1)) and matches:
+        if alist1[pos]==alist2[pos]:
+            pos = pos + 1
+        else:
+            matches = False
+    return matches
+
