9a1e22f1094e0b045e96962aa289c04ce9c5f673
Maciej Nalewajka <nalewajkamaciek@gmail.com>
2020-02-14 12:18:21 +0100

    Add Kolejka Priorytetowa.py

---
+++ Kolejka Priorytetowa.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class PQueue:
+
+    def __init__(self):
+        self.items = []
+
+    def __str__(self):
+        return str(self.items)
+
+    def is_empty(self):
+        return not self.items
+
+    def insert(self, item):
+        self.items.append(item)
+
+    def remove(self):
+        maxx = 0
+        for i in range(1, len(self.items)):
+            if self.items[i] > self.items[maxx]:
+                maxx = i
+        return self.items.pop(maxx) 
+
