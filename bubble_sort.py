de8fbafa900652487933a7cdb1bc8ddc6caf5c4f
Maciej Nalewajka <nalewajkamaciek@gmail.com>
2020-02-14 11:52:09 +0100

    Add bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def bubble_sort(lista):
+    for j in range(len(lista)-1, 0, -1):
+        for i in range(j):
+            if lista[i] > lista[i+1]:
+                lista[i], lista[i+1] = lista[i+1], lista[i]
+    return lista
+
